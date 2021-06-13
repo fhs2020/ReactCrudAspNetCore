663db4808a8c5e8c255be3488c56396f3f78a485
Flavio H Sousa <flavioh007@gmail.com>
2021-06-12 22:06:32 -0300

    First commit!

---
+++ WebFlavioReact/Config/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebFlavioReact.Model;
+
+namespace WebFlavioReact.Config
+{
+    public class Context : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
